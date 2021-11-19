6bdbb3855a54ea1a70b1f6048738d135d0b34641
Козюра Алексей Андреевич <kozyura_aleksey@mail.ru>
2021-11-19 16:32:49 +0300

    migrates and test data

--- dto/tag.js
+++ dto/tag.js
@@ -1,13 +1,6 @@
 export default class Tag {
-    /** @type {number} */
-    #id;
-    /** @type {uuid} */
-    #creator;
-    /** @type {string} */
-    #name;
-    #sortOrder;
     constructor(id, creator, name) {
-        this.#id = id;
+        this.id = id;
         this.creator = creator;
         this.name = name;
     }
